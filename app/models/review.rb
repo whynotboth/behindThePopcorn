2fb34c0484fe3a3012c652cd40625f19cf1e74fa
whynotboth <tkudlek@yahoo.com>
2017-12-09 14:50:45 -0600

    Added corrected model sorting technique inside the review model file. Sorted all reviews and added another 10-15 reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ApplicationRecord
   has_attached_file :image
   validates_attachment :image,
                      content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+  default_scope { order(title: :asc) }
 end
