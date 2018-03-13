a0997b3a4547ac194a9caa3a04a90a1684c06bdc
whynotboth <tkudlek@yahoo.com>
2018-03-13 17:32:20 -0500

    Changed puma.rb to match heroku's basic outline for web apps

--- config/puma.rb
+++ config/puma.rb
@@ -1,20 +1,15 @@
-if ENV['RACK_ENV'] != 'production' || ENV['RAILS_ENV'] != 'production'
-  workers Integer(ENV['PUMA_WORKERS'] || 4)
-end
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
 
-threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
-
-rackup      DefaultRackup
-port ENV['PORT'] || 3000
-environment ENV['RACK_ENV'] || 'development'
 preload_app!
 
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
 on_worker_boot do
-  # worker specific setup
-  ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-                Rails.application.config.database_configuration[Rails.env]
-    config['pool'] = ENV['MAX_THREADS'] || 16
-    ActiveRecord::Base.establish_connection(config)
-  end
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
 end
